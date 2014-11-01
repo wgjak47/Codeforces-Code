ca3130afaa2b6a21e61ff08539eb42820dd24d4c
wgjak47 <ak47m61@gmail.com>
2014-11-01 22:39:38 +0800

    finish problem C in contest 275

--- 275/C.cpp
+++ 275/C.cpp
@@ -11,7 +11,38 @@
 #define mt(a,d) memset((a),(d),sizeof(a))
 #define LLD long long
 using namespace std;
+void build_perfix(int n, int k)
+{
+    int l=2,r=n;
+    int reg=1;
+    cout<<1;
+    while (reg < k)
+    {
+        if (reg%2==0)
+        {
+            cout<<' '<<l++;
+        }
+        else 
+        {
+            cout<<' '<<r--;
+        }
+        reg++;
+    }
+    if (reg%2)
+    {
+        for (int i=l;i<=r;i++)
+            cout<<' '<<i;
+    }
+    else
+    {
+        for (int i=r;i>=l;i--)
+            cout<<' '<<i;
+    }
+}
 main()
 {
+    int n,k;
+    cin>>n>>k;
+    build_perfix(n,k);
 	return 0;
 }
